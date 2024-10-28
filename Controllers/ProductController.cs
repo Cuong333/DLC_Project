9e31f32e73f8ed9b9d890bd3e5ced5d1f87d3c16
cuong333 <dangcuong9331@gmail.com>
2024-10-28 21:32:02 +0700

    go to shop (Product)

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DLC_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace DLC_Project.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly DemoDlcContext _db;
+
+        // Injecting the database context using dependency injection
+        public ProductController(DemoDlcContext db)
         {
-            return View();
+            _db = db;
         }
 
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = page == null || page <= 0 ? 1 : page.Value;
+
+            var listProduct = _db.Products.AsNoTracking().OrderBy(x => x.ProductName);
+
+            // Apply pagination using X.PagedList
+            IPagedList<Product> pagedList = listProduct.ToPagedList(pageNumber, pageSize);
+
+            return View(pagedList);
+        }
+
+        // Product Detail action to view specific product details
         public IActionResult ProductDetail()
         {
             return View("~/Views/Product/ProductDetail.cshtml");
